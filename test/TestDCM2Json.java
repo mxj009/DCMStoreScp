09c0f02b23e72d1f650a450631e8b523a0e5c7f4
zhenxun815 <guoyiheng89@gmail.com>
2018-05-15 09:14:55 +0800

    新增测试解析TalkScuMsg方法

--- test/TestDCM2Json.java
+++ test/TestDCM2Json.java
@@ -1,7 +1,8 @@
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.tqhy.dcm4che.storescp.constant.DicomTag;
-import com.tqhy.dcm4che.storescp.entity.ImgCase;
+import com.tqhy.dcm4che.entity.ImgCase;
+import com.tqhy.dcm4che.msg.InitScuMsg;
 import com.tqhy.dcm4che.storescp.utils.StringUtils;
 import org.dcm4che3.io.DicomInputStream;
 import org.dcm4che3.json.JSONWriter;
@@ -90,6 +91,18 @@ public class TestDCM2Json {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void testParseTalkScuMsg(){
+        String msg = "{\"status\":1,\"desc\":\"message description\",\"part\":[{\"createTime\":1522222434000,\"delFlag\":1,\"id\":\"2c28c4b79af8445db766515c323a5aff\",\"name\":\"??\",\"updateTime\":1522222434000},{\"createTime\":1503968936000,\"delFlag\":1,\"id\":\"5b571a16d61b42f7a7f5e8b9076605f8\",\"name\":\"??\",\"updateTime\":1503968936000},{\"createTime\":1515566629000,\"delFlag\":1,\"id\":\"5bd8be85670d4cfba7f141e9fb050ec9\",\"name\":\"???\",\"updateTime\":1515566629000},{\"createTime\":1499826936000,\"delFlag\":1,\"id\":\"867d657ec68b447ab57f3dff6c3cf576\",\"name\":\"??\",\"updateTime\":1499826936000},{\"createTime\":1514343220000,\"delFlag\":1,\"id\":\"9f19e6bc41d148739a2d7b2dda228030\",\"name\":\"??\",\"updateTime\":1514343220000},{\"createTime\":1523177588000,\"delFlag\":1,\"id\":\"cab76a4dcc3b4b75977e2b9f59ad9031\",\"name\":\"ISIC\",\"updateTime\":1523177588000},{\"createTime\":1505125602000,\"delFlag\":1,\"id\":\"f530443b1a1947799638b15805f35269\",\"name\":\"??\",\"updateTime\":1505125602000}],\"source\":[{\"createTime\":1523177723000,\"delFlag\":1,\"id\":\"3525de9456e54607b5ccf0aad18920ec\",\"name\":\"???\",\"updateTime\":1523177723000},{\"createTime\":1499824161000,\"delFlag\":1,\"id\":\"3f8b081edd0c4060805bf6a077f30679\",\"name\":\"????\",\"updateTime\":1499824161000},{\"createTime\":1512365180000,\"delFlag\":1,\"id\":\"43172961c8eb44a1854499576af10db5\",\"name\":\"?????\",\"updateTime\":1512365180000},{\"createTime\":1505125718000,\"delFlag\":1,\"id\":\"66ddbafe669245f4b58c2c175f435e94\",\"name\":\"??\",\"updateTime\":1505125718000},{\"createTime\":1514342971000,\"delFlag\":1,\"id\":\"8310f5b97fe54a6495688263fa6ca928\",\"name\":\"????\",\"updateTime\":1514342971000},{\"createTime\":1515551362000,\"delFlag\":1,\"id\":\"87670ceda5ee42299258a9fdf5c361bd\",\"name\":\"????\",\"updateTime\":1515551362000},{\"createTime\":1522378201000,\"delFlag\":1,\"id\":\"c1d0878591104f28b2ca2e6dc4cc5bb9\",\"name\":\"????\",\"updateTime\":1522378201000},{\"createTime\":1503969044000,\"delFlag\":1,\"id\":\"dfbee262e15b46d8bc08d1532996bc15\",\"name\":\"??\",\"updateTime\":1503969044000},{\"createTime\":1500520307000,\"delFlag\":1,\"id\":\"e36fbde330594cd6a8d9e5c66551d12d\",\"name\":\"??\",\"updateTime\":1500520307000},{\"createTime\":1515565829000,\"delFlag\":1,\"id\":\"f887334808594a478f65a10925e8e601\",\"name\":\"????\",\"updateTime\":1515565829000}],\"type\":[{\"createTime\":1499826937000,\"delFlag\":1,\"id\":\"6c77cb00e3e743bc963ed71f1a2f5082\",\"name\":\"DR\",\"updateTime\":1499826937000},{\"createTime\":1508479424000,\"delFlag\":1,\"id\":\"de38edcc7ff1461ab8d2185ef6d66ad4\",\"name\":\"CT\",\"updateTime\":1508479424000}]}";
+        InitScuMsg initScuMsg = new Gson().fromJson(msg, InitScuMsg.class);
+
+        System.out.println(initScuMsg.getStatus());
+        System.out.println(initScuMsg.getDesc());
+        System.out.println(initScuMsg.getPart());
+        System.out.println(initScuMsg.getType());
+        System.out.println(initScuMsg.getSource());
     }
 
     private String getValue(Map.Entry<String, Map<String, Object>> entry) {
