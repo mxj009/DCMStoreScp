a35f811becd2e3625f779b55118472e11281a5e4
zhenxun815 <guoyiheng89@gmail.com>
2018-05-15 18:47:08 +0800

    修改:TRANSFER_ECXEL_REQUEST = 2005;TRANSFER_ECXEL_READY = 2006;

--- src/com/tqhy/dcm4che/msg/ScuCommandMsg.java
+++ src/com/tqhy/dcm4che/msg/ScuCommandMsg.java
@@ -9,11 +9,12 @@ public class ScuCommandMsg extends BaseMsg{
 
     private static final long serialVersionUID = 1L;
     private int command;
-    public static final int TRANSFER_ECXEL = 2005;
     public static final int GET_ALL_INIT_INFO = 2001;
     public static final int GET_PARTS = 2002;
     public static final int GET_SOURCES = 2003;
     public static final int GET_TYPES = 2004;
+    public static final int TRANSFER_ECXEL_REQUEST = 2005;
+    public static final int TRANSFER_ECXEL_READY = 2006;
 
     public ScuCommandMsg(int status) {
         super(status);
