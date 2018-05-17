26e0b8901969169fcb99a9d3fe48c31b66f77bcb
zhenxun815 <guoyiheng89@gmail.com>
2018-05-17 19:16:14 +0800

    新增CREATE_BATCH_REQUEST = 2007; CREATE_BATCH_READY = 2008; TRANSFER_DICOM_REQUEST = 2009; TRANSFER_DICOM_READY = 2010;

--- src/com/tqhy/dcm4che/msg/ScuCommandMsg.java
+++ src/com/tqhy/dcm4che/msg/ScuCommandMsg.java
@@ -6,7 +6,6 @@ package com.tqhy.dcm4che.msg;
  * @since 1.0.0
  */
 public class ScuCommandMsg extends BaseMsg{
-
     private static final long serialVersionUID = 1L;
     private int command;
     public static final int GET_ALL_INIT_INFO = 2001;
@@ -15,6 +14,10 @@ public class ScuCommandMsg extends BaseMsg{
     public static final int GET_TYPES = 2004;
     public static final int TRANSFER_ECXEL_REQUEST = 2005;
     public static final int TRANSFER_ECXEL_READY = 2006;
+    public static final int CREATE_BATCH_REQUEST = 2007;
+    public static final int CREATE_BATCH_READY = 2008;
+    public static final int TRANSFER_DICOM_REQUEST = 2009;
+    public static final int TRANSFER_DICOM_READY = 2010;
 
     public ScuCommandMsg(int status) {
         super(status);
