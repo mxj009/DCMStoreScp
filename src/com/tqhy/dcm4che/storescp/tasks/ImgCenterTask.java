5e2e1ace8217e4e1b6f33e1fc3c377cd42c0e485
zhenxun815 <guoyiheng89@gmail.com>
2018-05-23 18:59:28 +0800

    修改方法为静态调用

--- src/com/tqhy/dcm4che/storescp/tasks/ImgCenterTask.java
+++ src/com/tqhy/dcm4che/storescp/tasks/ImgCenterTask.java
@@ -1,5 +1,7 @@
 package com.tqhy.dcm4che.storescp.tasks;
 
+import com.tqhy.dcm4che.Main;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
@@ -13,39 +15,31 @@ import java.net.URL;
  * @since 1.0.0
  */
 public class ImgCenterTask {
-    private File dicomFile;
-    private File jpgFile;
 
-    public String getImgUrl() {
-        System.out.println(getClass().getSimpleName() + " getImgUrl...");
-        Dcm2JpgTask dcm2JpgTask = new Dcm2JpgTask(dicomFile);
-        jpgFile = dcm2JpgTask.call();
+    public static String getImgUrl(File dicomFile) {
+        System.out.println(ImgCenterTask.class.getSimpleName() + " getImgUrl...");
+        File jpgFile = Dcm2JpgTask.getInstance().convert(dicomFile);
         if (null != jpgFile && jpgFile.exists()) {
             return jpgFile.getPath();
         }
         return null;
     }
 
-    public String getImgUrlThumb() {
-        System.out.println(getClass().getSimpleName() + " getImgUrlThumb...");
-        return callPython("/imgUrlThumb.py", "_thumb.jpg");
+    public static String getImgUrlThumb(File jpgFile) {
+        System.out.println(ImgCenterTask.class.getSimpleName() + " getImgUrlThumb...");
+        return callPython("/imgUrlThumb.py", "_thumb.jpg", jpgFile);
     }
 
-    public String getImg1024Url() {
-        System.out.println(getClass().getSimpleName() + " getImg1024Url...");
-        return callPython("/img1024Url.py", "_1024.jpg");
+    public static String getImg1024Url(File jpgFile) {
+        System.out.println(ImgCenterTask.class.getSimpleName() + " getImg1024Url...");
+        return callPython("/img1024Url.py", "_1024.jpg", jpgFile);
     }
 
-    private String callPython(String pyResource, String target) {
+    private static String callPython(String pyResource, String target, File jpgFile) {
         if (null != jpgFile && jpgFile.exists() && jpgFile.isFile()) {
             String imgPath = jpgFile.getAbsolutePath();
-            URL resource = ImgCenterTask.class.getResource(pyResource);
-            String pyPath = resource.getPath();
-            System.out.println("path is: " + pyPath);
-            if ('/' == pyPath.charAt(0)) {
-                pyPath = pyPath.substring(1);
-                System.out.println("path is: " + pyPath);
-            }
+            String pyPath = Main.rootPath + pyResource;
+            System.out.println("python path is: " + pyPath);
             String[] arguments = new String[]{"python", pyPath, imgPath};
             try {
                 Process process = Runtime.getRuntime().exec(arguments);
@@ -53,13 +47,13 @@ public class ImgCenterTask {
                 BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
                 String line = null;
                 while ((line = in.readLine()) != null) {
-                    System.out.println(getClass().getSimpleName() + " callPython while.." + line);
+                    System.out.println(ImgCenterTask.class.getSimpleName() + " callPython while.." + line);
                     if ("ok".equals(line)) {
                         String newImgPath = imgPath.replace(".jpg", target);
-                        System.out.println(getClass().getSimpleName() + " callPython complete... " + newImgPath);
+                        System.out.println(ImgCenterTask.class.getSimpleName() + " callPython complete... " + newImgPath);
                         return newImgPath;
                     } else {
-                        System.out.println(getClass().getSimpleName() + " call Python fail...");
+                        System.out.println(ImgCenterTask.class.getSimpleName() + " call Python fail...");
                     }
                 }
                 in.close();
@@ -71,7 +65,4 @@ public class ImgCenterTask {
         return null;
     }
 
-    public ImgCenterTask(File dicomFile) {
-        this.dicomFile = dicomFile;
-    }
 }
