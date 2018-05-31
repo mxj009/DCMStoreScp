e081fada1c1ae8f8bee09c59d9965d6c6c5797f6
zhenxun815 <guoyiheng89@gmail.com>
2018-05-31 19:04:20 +0800

    1.新增文件MD5加密方法getMD5 2.新增通过路径字符串获取文件名方法getFileNameWithoutSuffix

--- src/com/tqhy/dcm4che/storescp/utils/FileUtils.java
+++ src/com/tqhy/dcm4che/storescp/utils/FileUtils.java
@@ -1,8 +1,10 @@
 package com.tqhy.dcm4che.storescp.utils;
 
 import com.tqhy.dcm4che.Main;
+import org.apache.commons.codec.binary.Hex;
 
 import java.io.*;
+import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -186,10 +188,45 @@ public class FileUtils {
                 paths.add(outFile.getAbsolutePath());
             }
             zis.close();
-            file.delete();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return paths;
+    }
+
+    /**
+     * 获取一个文件的md5值
+     *
+     * @return md5 value
+     */
+    public static String getMD5(File file) {
+        FileInputStream fileInputStream = null;
+        try {
+            MessageDigest MD5 = MessageDigest.getInstance("MD5");
+            fileInputStream = new FileInputStream(file);
+            byte[] buffer = new byte[8192];
+            int length;
+            while ((length = fileInputStream.read(buffer)) != -1) {
+                MD5.update(buffer, 0, length);
+            }
+            return new String(Hex.encodeHex(MD5.digest()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static String getFileNameWithoutSuffix(String path) {
+        String name = new File(path).getName();
+        name = name.substring(0, name.indexOf('.'));
+        return name;
     }
 }
